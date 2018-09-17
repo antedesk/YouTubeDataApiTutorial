3894b0019e06436abe2457d2810b4925cf1018c7
Antedesk <antedesk@gmail.com>
2018-09-18 00:42:32 +0200

    added youtube video player

--- app/src/main/java/it/antedesk/youtubedataapitutorial/MainActivity.java
+++ app/src/main/java/it/antedesk/youtubedataapitutorial/MainActivity.java
@@ -2,13 +2,48 @@ package it.antedesk.youtubedataapitutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private final String VIDEO_ID = "2-5Wv9UGkN8";
+
+    @BindView(R.id.youtube_player_view)
+    YouTubePlayerView youTubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // Initializing YouTube player view
+        youTubePlayerView.initialize(getString(R.string.youtube_data_api_key), this);
+    }
+
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer,
+                                        boolean wasRestored) {
+        if(null== youTubePlayer) return;
+
+        // Start buffering
+        if (!wasRestored) {
+            youTubePlayer.cueVideo(VIDEO_ID);
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        Toast.makeText(this, "Failed to initialize YouTube Video Player.", Toast.LENGTH_LONG).show();
+
     }
 }
