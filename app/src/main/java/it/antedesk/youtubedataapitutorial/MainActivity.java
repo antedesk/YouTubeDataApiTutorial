0bc640c76a6d746cea5324ec9a1dc4b5f3835573
Antedesk <antedesk@gmail.com>
2018-09-18 02:05:07 +0200

    added YouTubePlayerSupportFragment for displaying a youtube video

--- app/src/main/java/it/antedesk/youtubedataapitutorial/MainActivity.java
+++ app/src/main/java/it/antedesk/youtubedataapitutorial/MainActivity.java
@@ -1,14 +1,67 @@
 package it.antedesk.youtubedataapitutorial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+
+import butterknife.ButterKnife;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_REQUEST = 1;
+    private final String[] videos = {"bI31WqFDxNs", "20bpjtCbCz0", "D86RtevtfrA", "xZNBFcwd7zc", "2-5Wv9UGkN8", "Z5ezsReZcxU"};
+
+    private YouTubePlayerSupportFragment mYuoTubePlayerFrag;
+    private YouTubePlayer mYouTubePlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // finding UI elements
+        mYuoTubePlayerFrag =
+                (YouTubePlayerSupportFragment) getSupportFragmentManager().findFragmentById(R.id.youtube_fragment);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mYuoTubePlayerFrag.initialize(getString(R.string.youtube_data_api_key), this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer,
+                                        boolean wasRestored) {
+        if (!wasRestored) {
+            mYouTubePlayer = youTubePlayer;
+            youTubePlayer.cueVideo(videos[0]);
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult youTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), youTubeInitializationResult.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RECOVERY_REQUEST) {
+            getSupportFragmentManager().findFragmentById(R.id.youtube_fragment);
+        }
     }
 }
